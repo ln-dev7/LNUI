a3c64e59ba6bae369b2c21ba774ca36770bf656d
LN <62269693+ln-dev7@users.noreply.github.com>
2023-03-13 16:35:55 +0100

    add alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link.js";
 import Footer from "../components/website/Footer";
@@ -6,84 +7,115 @@ import Header from "../components/website/Header";
 import { sponsors } from "../data/sponsor.js";
 
 export default function Home() {
+  const [showAlert, setShowAlert] = useState(true);
   return (
-    <div>
-      <Head>
-        <title>LNUI</title>
-        <meta
-          name="description"
-          content="Create beautiful websites with multiple components"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Header />
-
-      <main className="homePage">
-        <section className="hero">
-          <div className="hero__lines"></div>
-          <div className="hero__container">
-            <h1>
-              Create <span>beautiful</span> websites with multiple{" "}
-              <span>components</span>
-            </h1>
-            <p>
-              With <span>LNUI</span> you will have components of all types that
-              will allow you to build your website from scratch
-            </p>
-            <div className="hero__container-button">
-              <Link href="/intro" className="hero__container-button__first">
-                <span>Discover</span>
-              </Link>
-              <Link
-                href="/templates"
-                className="hero__container-button__second"
-              >
-                <span>Explore templates</span>
-              </Link>
-            </div>
-          </div>
-        </section>
-        <div className="sponsors">
-          <h2 className="sponsors-title">Those who support us </h2>
-          <div className="sponsors__container">
-            <div className="sponsors-logos">
-              {sponsors.map((item) => {
-                return (
-                  <Link href={item.url} key={item.name} target="_blank">
-                    <Image
-                      loader={({ src }) => src}
-                      src={item.logo}
-                      alt="logo"
-                      width={300}
-                      height={70}
-                    />
-                  </Link>
-                );
-              })}
-            </div>
-          </div>
-          <a className="sponsors-add" href="mailto:me@leonelngoya.com">
-            Your logo here?
-          </a>
+    <>
+      {showAlert && (
+        <div className="lnui-alert">
+          <p>
+            We are still in the development phase, 0.1 will be released soon ...
+          </p>
+          <button onClick={() => setShowAlert(false)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
         </div>
-        {false && (
-          <div className="newsletter">
-            <div className="newsletter__container">
-              <h2>Subscribe to our newsletter</h2>
+      )}
+      <div
+        style={{
+          position: "relative",
+        }}
+      >
+        <Head>
+          <title>LNUI</title>
+          <meta
+            name="description"
+            content="Create beautiful websites with multiple components"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Header />
+
+        <main className="homePage">
+          <section className="hero">
+            <div className="hero__lines"></div>
+            <div className="hero__container">
+              <h1>
+                Create <span>beautiful</span> websites with multiple{" "}
+                <span>components</span>
+              </h1>
               <p>
-                Receive the latest news and updates from LNUI and our community
+                With <span>LNUI</span> you will have components of all types
+                that will allow you to build your website from scratch
               </p>
-              <div>
-                <input type="email" />
-                <button type="submit">Subscribe</button>
+              <div className="hero__container-button">
+                <Link href="/intro" className="hero__container-button__first">
+                  <span>Discover</span>
+                </Link>
+                <Link
+                  href="/templates"
+                  className="hero__container-button__second"
+                >
+                  <span>Explore templates</span>
+                </Link>
               </div>
             </div>
+          </section>
+          <div className="sponsors">
+            <h2 className="sponsors-title">Those who support us </h2>
+            <div className="sponsors__container">
+              <div className="sponsors-logos">
+                {sponsors.map((item) => {
+                  return (
+                    <Link href={item.url} key={item.name} target="_blank">
+                      <Image
+                        loader={({ src }) => src}
+                        src={item.logo}
+                        alt="logo"
+                        width={300}
+                        height={70}
+                      />
+                    </Link>
+                  );
+                })}
+              </div>
+            </div>
+            <a className="sponsors-add" href="mailto:me@leonelngoya.com">
+              Your logo here?
+            </a>
           </div>
-        )}
-      </main>
+          {false && (
+            <div className="newsletter">
+              <div className="newsletter__container">
+                <h2>Subscribe to our newsletter</h2>
+                <p>
+                  Receive the latest news and updates from LNUI and our
+                  community
+                </p>
+                <div>
+                  <input type="email" />
+                  <button type="submit">Subscribe</button>
+                </div>
+              </div>
+            </div>
+          )}
+        </main>
 
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </>
   );
 }
