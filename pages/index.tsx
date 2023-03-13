b1d9c818a8f0fbe0897369f6bd09c751bd78e4ef
LN <62269693+ln-dev7@users.noreply.github.com>
2023-03-13 16:49:55 +0100

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,32 +7,13 @@ import Header from "../components/website/Header";
 import { sponsors } from "../data/sponsor.js";
 
 export default function Home() {
-  const [showAlert, setShowAlert] = useState(true);
   return (
     <>
-      {showAlert && (
-        <div className="lnui-alert">
-          <p>
-            We are still in the development phase, 0.1 will be released soon ...
-          </p>
-          <button onClick={() => setShowAlert(false)}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-          </button>
-        </div>
-      )}
+      <div className="lnui-alert">
+        <p>
+          We are still in the development phase, 0.1 will be released soon ...
+        </p>
+      </div>
       <div
         style={{
           position: "relative",
